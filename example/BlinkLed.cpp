c2ca49cbb690af1cc27397b66f7a47e7437defa1
ahb <andreas.bihlmaier@gmx.de>
2014-08-22 14:05:55 +0200

    does not really belong here ... extend example by CAN

--- example/BlinkLed.cpp
+++ example/BlinkLed.cpp
@@ -6,9 +6,13 @@
 
 DigitalOut myled(LED1);
 Serial pc(USBTX, USBRX);
+CAN can(p30, p29);
 
 int main() {
   int i = 0;
+
+  can.frequency(125000);
+
   while(1) {
     myled = 1;
     wait(0.3);
@@ -16,5 +20,14 @@ int main() {
     wait(0.3);
     pc.printf("i: %i\n\r", i);
     ++i;
+
+    if (!can.write(CANMessage(1337, (char*)&i, 4))) {
+      pc.printf("CAN send failed\n\r");
+    }
+
+    CANMessage msg;
+    if(can.read(msg)) {
+      pc.printf("CAN recv: 0x%02X\n\r", msg.data[0]);
+    }
   }
 }
